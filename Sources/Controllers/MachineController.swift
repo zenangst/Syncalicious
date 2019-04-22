40907d9a107848a1f5f8b03dead0f82b0e6060d9
Christoffer Winterkvist <christoffer@winterkvist.com>
2019-04-22 19:35:05 +0200

    Improve idle syncing

--- Sources/Controllers/MachineController.swift
+++ Sources/Controllers/MachineController.swift
@@ -20,8 +20,9 @@ class MachineController {
 
   let machine: Machine
   let shellController: ShellController
-  var threshold: Float = 5
+  var threshold: Double = 60
   var timer: Timer?
+  private(set) var state: State = .active
 
   public init(host: Host, shellController: ShellController) throws {
     self.shellController = shellController
@@ -36,7 +37,7 @@ class MachineController {
     self.machine = Machine(name: name,
                            localizedName: localizedName)
 
-    let timer = Timer.init(timeInterval: 5.0, repeats: true, block: { [weak self] _ in
+    let timer = Timer.init(timeInterval: 30.0, repeats: true, block: { [weak self] _ in
       self?.checkIdleState()
     })
     RunLoop.current.add(timer, forMode: .common)
@@ -56,14 +57,35 @@ class MachineController {
     return destination.appendingPathComponent(machine.name)
   }
 
+  // Implementation is based of this gist.
+  // https://gist.github.com/darrarski/c586ce379d3e7e91a57d89ed557192ec
+  private func systemIdleTime() -> Double? {
+    var iterator: io_iterator_t = 0
+    defer { IOObjectRelease(iterator) }
+    guard IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching("IOHIDSystem"), &iterator) == KERN_SUCCESS else {
+      return nil }
+
+    let entry: io_registry_entry_t = IOIteratorNext(iterator)
+    defer { IOObjectRelease(entry) }
+    guard entry != 0 else { return nil }
+
+    var unmanagedDict: Unmanaged<CFMutableDictionary>?
+    defer { unmanagedDict?.release() }
+    guard IORegistryEntryCreateCFProperties(entry, &unmanagedDict, kCFAllocatorDefault, 0) == KERN_SUCCESS,
+      let dict = unmanagedDict?.takeUnretainedValue() else { return nil }
+
+    let key: CFString = "HIDIdleTime" as CFString
+    let value = CFDictionaryGetValue(dict, Unmanaged.passUnretained(key).toOpaque())
+    let number: CFNumber = unsafeBitCast(value, to: CFNumber.self)
+    var nanoseconds: Int64 = 0
+    guard CFNumberGetValue(number, CFNumberType.sInt64Type, &nanoseconds) else { return nil }
+    return Double(nanoseconds) / Double(NSEC_PER_SEC)
+  }
+
   private func checkIdleState() {
-    let command = """
-  ioreg -c IOHIDSystem | awk '/HIDIdleTime/ {print $NF/1000000000; exit}'
-"""
-
-    guard let output = Float(shellController.execute(command: command)) else { return }
-    let state: State = output > threshold ? .idle : .active
-
+    let idleTime = systemIdleTime() ?? 0
+    let state: State = idleTime > threshold ? .idle : .active
+    self.state = state
     delegate?.machineController(self, didChangeState: state)
   }
 }
